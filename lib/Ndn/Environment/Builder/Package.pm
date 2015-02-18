8108dcfe3bdabcbcf07e5400d83b6a938ddc1223
Chris Weyl <chris.weyl@dreamhost.com>
2015-02-18 11:19:37 -0800

    package builder: rejigger how we build debian/control
    We're going to need to start creating additional file under the debian/
    directory, so let's do this refactoring up front so we can take advantage of
    it *and* see that everything works here before moving on.
    
    (...or "DEBIAN/", as it's known at the moment.)

--- lib/Ndn/Environment/Builder/Package.pm
+++ lib/Ndn/Environment/Builder/Package.pm
@@ -5,6 +5,10 @@ use warnings;
 use Ndn::Environment qw/builder/;
 use Ndn::Environment::Config;
 
+use Data::Section::Simple 'get_data_section';
+use Path::Tiny;
+use Template::Tiny;
+
 sub description {
     return "Package the environment.";
 }
@@ -36,46 +40,59 @@ sub steps {
     my $deps  = $self->args->{'depends=s'}     || config->{package}->{depends};
     my $ver   = $self->args->{'version=s'}     || config->{package}->{version}->();
 
+    my $debian_dir = path $pkg_dir, 'DEBIAN';
+
     chomp(my $arch = `uname -m`);
     $arch = 'amd64' if $arch eq 'x86_64';
 
-    $deps = "\nDepends: $deps" if $deps;
+    my $_build_control = sub {
+        $self->_debian_file($debian_dir => 'control', {
+            name  => $name,
+            maint => $maint,
+            arch  => $arch,
+            ver   => $ver,
+            deps  => $deps,
+        });
+    };
 
     return (
         sub {
             return unless config->{package}->{prebuild};
             config->{package}->{prebuild}->();
         },
+        sub { $debian_dir->mkpath },
         "mkdir -p $pkg_dir/$base_dir",
         "mkdir -p $pkg_dir/$dest",
         "rsync -avP '$dest' '$pkg_dir/$base_dir/'",
         "rm -rf '$dest'",
-#        "cd '$pkg_dir/$base_dir/'; ln -s $build_dir/* ./",
-        sub {
-            mkdir("$pkg_dir/DEBIAN");
-            open( my $fh, '>', "$pkg_dir/DEBIAN/control" )
-                || die "Could not create control file: $!";
+        sub { $_build_control->() },
 
-            print $fh <<"            EOT";
-Package: $name
-Priority: optional
-Section: devel
-Installed-Size: 100
-Maintainer: $maint
-Architecture: $arch
-Version: ${ver}${deps}
-Description: $desc
-            EOT
-
-            close($fh);
-        },
+        # FIXME note, this should probably be debuild
         "dpkg-deb -z8 -Zgzip --build '$pkg_dir' '$name-$ver.deb'"
     );
 }
 
-1;
+sub _debian_file {
+    my ($self, $debian_dir, $target_filename, $vars) = @_;
 
-__END__
+
+    ### get our template, and generate: "debian/$target_filename"
+    my $file     = path $debian_dir, $target_filename;
+    my $template = get_data_section("debian/$target_filename");
+    my $tt       = Template::Tiny->new;
+    $tt->process(
+        \$template,
+        $vars,
+        \my $output,
+    );
+
+    ### write: "$file"
+    $file->spew($output);
+
+    return;
+}
+
+1;
 
 =head1 NAME
 
@@ -91,3 +108,16 @@ NDN-Environment is distributed in the hope that it will be useful, but WITHOUT
 ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
 FOR A PARTICULAR PURPOSE. See the license for more details.
 
+=cut
+
+__DATA__
+
+@@ debian/control
+Package:      [% name %]
+Priority:     optional
+Section:      devel
+Maintainer:   [% maint %]
+Architecture: [% arch %]
+Version:      [% ver  %]
+Depends:      ${shlibs:Depends}, [% deps %]
+Description:  [% desc %]
